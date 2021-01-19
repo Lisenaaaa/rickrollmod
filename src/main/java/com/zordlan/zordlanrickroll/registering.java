4d8b5387b76a85c9dc0051eb054ba378c9eef10a
Zordlan <69115669+Zordlan@users.noreply.github.com>
2021-01-19 12:51:53 -0800

    thank you asbyth

--- src/main/java/com/zordlan/zordlanrickroll/registering.java
+++ src/main/java/com/zordlan/zordlanrickroll/registering.java
@@ -19,34 +19,47 @@ public class registering {
                 Minecraft.getMinecraft().gameSettings.showDebugInfo) {
             return;
         }
-        // use drawStringWithShadow for shadowed text
-        Minecraft.getMinecraft().fontRendererObj.drawString("We're no strangers to love", 2, 2, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("You know the rules and so do I", 2, 12, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("A full commitment's what I'm thinking of", 2, 22, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("You wouldn't get this from any other guy", 2, 32, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("I just wanna tell you how I'm feeling", 2, 44, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Gotta make you understand", 2, 54, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna give you up", 2, 66, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna let you down", 2, 76, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna run around and desert you", 2, 86, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna make you cry", 2, 96, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna say goodbye", 2, 106, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna tell a lie and hurt you", 2, 116, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("We've known each other for so long", 2, 128, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Your heart's been aching but you're too shy to say it", 2, 138, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Inside we both know what's been going on", 2, 148, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("We know the game and we're gonna play it", 2, 158, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("And if you ask me how I'm feeling", 2, 170, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Don't tell me you're too blind to see", 2, 180, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna give you up", 2, 192, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna let you down", 2, 202, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna run around and desert you", 2, 212, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna make you cry", 2, 222, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna say goodbye", 2, 232, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("Never gonna tell a lie and hurt you", 2, 242, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("No, I'm never gonna give you up", 2, 256, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("No, I'm never gonna let you down", 2, 266, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("No, I'll never run around and hurt you", 2, 276, -1 ,true);
-        Minecraft.getMinecraft().fontRendererObj.drawString("I'll never, ever desert you", 2, 286, -1 ,true);
+
+        Minecraft.getMinecraft().fontRendererObj.drawSplitString("We're no strangers to love\n" +
+                "You know the rules and so do I\n" +
+                "A full commitment's what I'm thinking of\n" +
+                "You wouldn't get this from any other guy\n\n" +
+                "I just wanna tell you how I'm feeling\n" +
+                "Gotta make you understand\n\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n\n" +
+                "We've known each other for so long\n" +
+                "Your heart's been aching but you're too shy to say it\n" +
+                "Inside we both know what's been going on\n" +
+                "We know the game and we're gonna play it\n\n" +
+                "And if you ask me how I'm feeling\n" +
+                "Don't tell me you're too blind to see\n\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n\n" +
+                "No, I'm never gonna give you up\n" +
+                "No, I'm never gonna let you down\n" +
+                "No, I'll never run around and hurt you\n" +
+                "Never, ever desert you\n\n" +
+                "We've known each other for so long\n" +
+                "Your heart's been aching but\n\n" +
+                "Never gonna give you up\n" +
+                "Never gonna let you down\n" +
+                "Never gonna run around and desert you\n" +
+                "Never gonna make you cry\n" +
+                "Never gonna say goodbye\n" +
+                "Never gonna tell a lie and hurt you\n\n" +
+                "No, I'm never gonna give you up\n" +
+                "No, I'm never gonna let you down\n" +
+                "No, I'll never run around and hurt you\n" +
+                "I'll never, ever desert you", 2, 2, 500, -1);
+
     }
 }
